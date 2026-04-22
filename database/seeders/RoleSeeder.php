564d5255ab4f8aa593368c58a2b872f89d5a1e73
David <n00327270@upn.pe>
2026-04-22 16:19:09 -0500

    Instalar Spatie Laravel Permission y crear seeder de roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $roles = ['admin', 'collaborator', 'adopter', 'surrenderer'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role, 'guard_name' => 'web']);
+        }
+    }
+}
